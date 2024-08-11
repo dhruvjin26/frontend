c9e4dcba4461ba0d450fddc6b376d7f553d71f3f
dhruvjin26 <jindaldhruv2607@gmail.com>
2024-08-11 20:00:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api' :'http://localhost:3000',
+      '/api' :'https://backend-nsre.onrender.com',
     }
   },
   plugins: [react()],
